0d47d1746c30e8c1d5c534823c12b059cb99b975
Jealyang <jaelyangchoi@gmail.com>
2025-02-28 22:15:00 +0900

    Initial commit

---
+++ src/test/java/org/example/redisinspring/RedisInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.redisinspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisInSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
