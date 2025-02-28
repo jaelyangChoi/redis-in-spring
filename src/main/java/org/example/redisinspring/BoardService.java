12be71cd80b5bd97087adbbf587add619f941691
Jealyang <jaelyangchoi@gmail.com>
2025-02-28 23:41:47 +0900

    feat: "Redis 적용"

--- src/main/java/org/example/redisinspring/BoardService.java
+++ src/main/java/org/example/redisinspring/BoardService.java
@@ -1,5 +1,6 @@
 package org.example.redisinspring;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +16,10 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+    //cacheNames: 캐시 이름을 설정
+    //key: Redis에 저장할 Key의 이름을 설정
+    //cacheManager: 사용할 cacheManager의 Bean 이름을 지정
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:'+ #page + ':size:' + #size", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
